020a236fd486fa0aa7c0212f883dbdc087b60ef3
Richard Arcega <138342305+richardarcega@users.noreply.github.com>
2023-07-09 18:51:03 -0400

    Adds antialiasing

--- main.cc
+++ main.cc
@@ -3,6 +3,7 @@
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
+#include "camera.h"
 
 #include <iostream>
 color ray_color(const ray& r, const hittable& world) {
@@ -21,6 +22,7 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
 
     // World
     hittable_list world;
@@ -28,6 +30,7 @@ int main() {
     world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
     // Camera
+    camera cam;
 
     auto viewport_height = 2.0;
     auto viewport_width = aspect_ratio * viewport_height;
@@ -45,11 +48,14 @@ int main() {
     for (int j = image_height - 1; j >= 0; --j) {
         std::cerr << "\nScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto u = double(i) / (image_width-1);
-            auto v = double(j) / (image_height-1);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            color pixel_color = ray_color(r, world);
-            write_color(std::cout, pixel_color);
+            color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / (image_width-1);
+                auto v = (j + random_double()) / (image_height-1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world);
+            }
+            write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
 
